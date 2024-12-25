d919d7f8ef620bb1837c4550085270a01972eb12
Roky360 <edenshkd@gmail.com>
2024-12-25 19:51:04 +0200

    Added check for env variables

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ import cors from "cors";
 import mongoose from "mongoose";
 import {env} from "custom-env";
 
+// check app variables
 env(process.env.NODE_ENV, "./config");
+if (process.env.MONGODB_URI === undefined) {
+    console.error("MongoDB URI is missing");
+    process.exit(1);
+}
+if (process.env.PORT === undefined) {
+    console.error("App port is missing");
+    process.exit(1);
+}
 
 // connect to mongodb
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
 });
 
+// setup app
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
