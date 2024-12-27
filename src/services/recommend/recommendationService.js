475d43beb1b1df781e3b343cd361d80d2a68a8a6
Roky360 <edenshkd@gmail.com>
2024-12-27 17:18:10 +0200

    Added support to watched movie array in User.

--- src/services/recommend/recommendationService.js
+++ src/services/recommend/recommendationService.js
@@ -18,13 +18,14 @@ exports.generateRecomId = async () => {
 }
 
 /**
- * Marks a movie as watched by a user in the recommendation system.
+ * Marks a movie as watched by a user in the recommendation system; also updates the user's watched array.
  * @param userId Recommendation user id.
  * @param movieId Recommendation movie id.
  * @returns void
  * @throws Error if user/movie does not exist.
  */
 exports.markAsWatched = async (userId, movieId) => {
+    // get user and movies docs and check they exist
     const userDoc = User.findById(userId);
     if (userDoc === null) {
         throw new Error('User does not exist');
@@ -35,14 +36,45 @@ exports.markAsWatched = async (userId, movieId) => {
     // TODO: get movie recom id the same way
     const movieRecomId = null;
 
-    // decide whether to send POST or PATCH request
-    if (userDoc.get('first_watch') === true) {
-        // it's the user's first watch - POST
-        userDoc.updateOne({'first_watch': false});
-        await sendRequest(`POST ${userRecomid} ${movieRecomId}`);
-    } else {
-        // PATCH
-        await sendRequest(`PATCH ${userRecomid} ${movieRecomId}`);
+    try {
+        // decide whether to send POST or PATCH request
+        if (userDoc.get('first_watch') === true) {
+            // it's the user's first watch - POST
+            userDoc.updateOne({'first_watch': false});
+            await sendRequest(`POST ${userRecomid} ${movieRecomId}`);
+        } else {
+            // PATCH
+            await sendRequest(`PATCH ${userRecomid} ${movieRecomId}`);
+        }
+        // add the movie id to the watched array of the user
+        await userDoc.updateOne({$push: {'movies': movieId}});
+    } catch (err) {
+    }
+}
+
+/**
+ * Marks a movie as unwatched by a user. Sends a request to the recommendation server and updates the user's watched
+ * array.
+ * @param userId
+ * @param movieId
+ * @returns {Promise<void>}
+ */
+exports.markAsUnwatched = async (userId, movieId) => {
+    // get user and movies docs and check they exist
+    const userDoc = User.findById(userId);
+    if (userDoc === null) {
+        throw new Error('User does not exist');
+    }
+    // TODO: make sure the movie exist, similar to this ^
+
+    const userRecomid = userDoc.get('recom_id');
+    // TODO: get movie recom id the same way
+    const movieRecomId = null;
+
+    try {
+        await sendRequest(`DELETE ${userRecomid} ${movieRecomId}`);
+        await userDoc.updateOne({$pull: {'movies': movieId}});
+    } catch (err) {
     }
 }
 
@@ -60,8 +92,4 @@ exports.recommend = async (userId, movieId) => {
         res.payload = res.payload.split(' '); // convert the recommended movie list to an array
     }
     return res;
-}
-
-exports.markAsUnwatched = async (userId, movieId) => {
-    return sendRequest(`DELETE ${userId} ${movieId}`);
 }
