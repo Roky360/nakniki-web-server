307f38a87b9f10047d0b64ea27a93ac17e15682d
KfirEitan15 <kfireitan115@gmail.com>
2024-12-31 18:03:42 +0200

    some fixes after testing (#22)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,22 @@
 const userService = require('../services/userService');
 
 const createUser = async (req, res) => {
-    try {
-        // create the user using the user service
-        const user = await userService.createUser(
-            req.body.username,
-            req.body.password,
-            req.body.email,
-            req.body.profile_pic
-        );
-        // return status 201 created
-        return res.status(201).json(user);
-    } catch (error) {
-        // if there was error return error message
-        return res.status(400).json({ errors: [error.message] });
+
+    // create the user using the user service
+    const user = await userService.createUser(
+        req.body.username,
+        req.body.password,
+        req.body.email,
+        req.body.profile_pic
+    );
+
+    if (!user.success) {
+        // if there was a problem return the error
+        return res.status(404).json({ error : [user.message] });
     }
+
+    // return status 201 created
+    return res.status(201).json(user.user);
 };
 
 const getUserById = async (req, res) => {
@@ -38,22 +40,16 @@ const isUserExist = async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    try {
-        // check if the user exist
-        const userExists = await userService.isUserExist(username, password);
-
-        if (userExists) {
-            // f user exist get him and return the user id
-            const user = await userService.getUserByUsernameAndPassword(username, password);
-            return res.status(200).json({ userId: user._id });
-        } else {
-            // if user doesnt exist return error message
-            return res.status(404).json({ errors: ['User not found'] });
-        }
-    } catch (error) {
-        // if there was error return error message
-        return res.status(400).json({ errors: ['Internal server error: ' + error.message] });
+    // check if the user exist
+    const userExist = await userService.isUserExist(username, password);
+    if (!userExist.success) {
+        // if the user not exist
+        return res.status(404).json({ error : [userExist.message] });
     }
+
+    // get the user and return his id
+    const user = await userService.getUserByUsernameAndPassword(username, password);
+    return res.status(200).json({ userId: user._id });
 };
 
 module.exports = {createUser, getUserById, isUserExist};
