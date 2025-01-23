d5c51e124ea401a9d6143c66d11c6e936bacec52
Roky360 <edenshkd@gmail.com>
2025-01-23 18:09:33 +0200

    fixed put movie endpoint in /movies and /uploads

--- src/controllers/uploadsController.js
+++ src/controllers/uploadsController.js
@@ -1,4 +1,4 @@
-const {UPLOADS_DIR, MOVIES_DIR} = require('../services/uploadsService');
+const {MOVIES_DIR} = require('../services/uploadsService');
 const path = require('path');
 const fs = require('fs');
 
@@ -33,55 +33,20 @@ const getVideo = async (req, res) => {
 }
 
 const putVideo = async (req, res) => {
-    const videoId = req.params.id;
-    const videoPath = path.join(MOVIES_DIR, videoId);
-
     if (!req.file) {
         return res.status(400).json({error: 'No video file provided.'});
     }
-
-    // Check if the original file exists
-    fs.access(videoPath, fs.constants.F_OK, (err) => {
-        if (err) {
-            return res.status(404).json({error: 'Original video not found.'});
-        }
-
-        // Replace the file with the new one
-        fs.unlink(videoPath, (err) => {
-            if (err) {
-                return res.status(500).json({error: 'Error replacing the video.'});
-            }
-
-            // Rename the uploaded file to the original file's name
-            fs.rename(req.file.path, videoPath, (err) => {
-                if (err) {
-                    return res.status(500).json({error: 'Error saving the new video file.'});
-                }
-                // replace was successful
-                res.status(204).json({});
-            });
-        });
-    });
+    res.status(204).json({});
 }
 
 const deleteVideo = async (req, res) => {
     const videoId = req.params.id;
     const videoPath = path.join(MOVIES_DIR, videoId);
 
-    // check if the file exists
-    fs.access(videoPath, fs.constants.F_OK, (err) => {
-        if (err) {
-            return res.status(404).json({error: 'Video not found.'});
-        }
-
-        // delete the file
-        fs.unlink(videoPath, (err) => {
-            if (err) {
-                return res.status(500).json({error: 'Error deleting the video.'});
-            }
-             // deleted successfully, return the deleted video (movie) id
-            res.status(200).json({videoId: videoId});
-        });
+    // delete the video
+    fs.unlink(videoPath, _ => {
+        // return the deleted video (movie) id
+        res.status(200).json({videoId: videoId});
     });
 }
 
